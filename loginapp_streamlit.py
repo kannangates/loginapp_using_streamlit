3262079827f5226c7281377733c4ccdde3ba68ac
Kannan <kannangates@gmail.com>
2021-06-19 01:23:27 +0530

    minor

--- loginapp_streamlit.py
+++ loginapp_streamlit.py
@@ -1,100 +1,62 @@
-import sqlite3 as sql3
 import streamlit as st
-import streamlit.components.v1 as stc
-
-HTML_BANNER = """
-    <div style="background-color:#567e5f;padding:3px;border-radius:10px">
-    <h1 style="color:white;text-align:center;">Select Menu from Sidebar for Navigation</h1>
-    </div>
-    """
-stc.html(HTML_BANNER)
+import os
+from pydub import AudioSegment
+import wave
+from google.cloud import storage
 
 
-# DB Management
-conn = sql3.connect("usersdata.db")
-c = conn.cursor()
+is_check = st.checkbox("Unlock Audio file conversion")
+
+if is_check:
+    audio_file_name = st.file_uploader(
+        "Upload Your Audio File", type=['mp3', 'wav'])
+    if audio_file_name is not None:
+        file_details = {"FileName": audio_file_name.name,
+                        "FileType": audio_file_name.type, "FileSize": audio_file_name.size}
+        # st.write(file_details)
+
+# Converting mp3 to wav format
 
 
-def create_usertable():
-    sql = '''CREATE TABLE IF NOT EXISTS userstable (username VARCHAR(30) unique ,password VARCHAR(30),mobile_number INTEGER PRIMARY KEY,gender VARCHAR(6))'''
-    c.execute(sql)
+def mp3_to_wav(audio_file_name):
+    if audio_file_name.split('.')[1] == 'mp3':
+        sound = AudioSegment.from_mp3(audio_file_name)
+        audio_file_name = audio_file_name.split('.')[0] + '.wav'
+        sound.export(audio_file_name, format="wav")
+
+# Converting Frame_rate
 
 
-def add_userdata(username, password, mobile_number, gender):
-    # def add_userdata(username, password):
-    c.execute('INSERT INTO userstable(username,password) VALUES (?,?,?,?)',
-              (username, password, mobile_number, gender))
+def frame_rate_channel(audio_file_name):
+    with wave.open(audio_file_name, "rb") as wave_file:
+        frame_rate = wave_file.getframerate()
+        print("\n\n** FILE CONVERSION SUCCESSFUL **")
+        channels = wave_file.getnchannels()
+        return frame_rate, channels
+
+# Converting stereo to mono, i.e. changing to 1 channel
 
 
-def login_user(username, password):
-    st.sidebar.write("Checking...")
-    c.execute('SELECT * FROM userstable WHERE username = ? and password = ?',
-              (username, password))
-    data = c.fetchall()
-    return data
+def stereo_to_mono(audio_file_name):
+    sound = AudioSegment.from_wav(audio_file_name)
+    sound = sound.set_channels(1)
+    sound.export(audio_file_name, format="wav")
 
 
-def view_all_user(username):
-    c.execute("Select * from userstable")
-    data = c.fetchall()
-    return data
+# Uploading audio file to Google Cloud
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+# Deleting audio file
 
 
-def user_details(username):
-    c.execute("Select * from userstable where username=?", (username))
-    data = c.fetchall()
-    return data
-
-
-def main():
-    """Simple Login Page"""
-
-    menu = ["Home", "Login", "Signup"]
-    choice = st.sidebar.selectbox("Menu", menu)
-
-    if choice == "Home":
-        st.header("Home Page")
-        st.text(f"You are currently in {choice} Page")
-
-    elif choice == "Login":
-        st.sidebar.header("Enter your Login Details")
-        st.text(f"You are currently in {choice} Page")
-        username = st.sidebar.text_input("User Name")
-        password = st.sidebar.text_input("Password", type="password")
-        login_button = st.sidebar.checkbox("Stay Logged In")
-        if login_button:
-            create_usertable()
-            # if password == "1234":
-            result = login_user(username, password)
-            if result:
-                st.sidebar.success(
-                    f"Login Successful, Logged in as {username}")
-                task = st.selectbox(
-                    "Task", ["To-Do", "Analytics", "Profile"])
-                if task == "To-Do":
-                    st.subheader("Add Your Task")
-                elif task == "Analytics":
-                    st.subheader("Analytics")
-                elif task == ("Profile"):
-                    st.subheader("Profile")
-            else:
-                st.sidebar.warning("Invaild Password/User name")
-
-    elif choice == "Signup":
-        st.header("Creat a New Account")
-        st.text(f"You are currently in {choice} Page")
-        new_user = st.text_input("Username", "")
-        new_password = st.text_input("Password", type="password")
-        new_user_mobile = st.text_input("Mobile Number", max_chars=10)
-        new_user_gender = st.text_input("Gender: Male , Female or Others")
-
-        if st.button("Sign Up"):
-            create_usertable()
-            add_userdata(new_user, new_password,
-                         new_user_mobile, new_user_gender)
-            st.success("Account Created Successfully")
-            st.info("Goto menu and Select Login")
-
-
-if __name__ == "__main__":
-    main()
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.delete()
